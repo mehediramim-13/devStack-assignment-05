28161f824bd289e279b1e9ca53db7d3961a2403a
mehediramim-13 <fuegoramim13@gmail.com>
2026-09-11 19:08:17 +0600

    Navigation Bar Implemented

---
+++ src/assets/Components/Nav.tsx
@@ -0,0 +1,32 @@
+import Logo from '../images/logo-text.png'
+
+const Nav = () => {
+    return (
+        <nav className='w-full bg-[#FFFEFE] sticky top-0 z-50'>
+            <div className='container mx-auto flex justify-between items-center h-[70px] px-4 font-jakarta'>
+
+                <div className='ml-20'>
+                    <img src={Logo} alt={"Logo"} />
+                </div>
+
+                <div>
+                    <ul className='flex justify-between gap-6 '>
+                        <li className='text-[#db2777FF]'><a href="">Home</a></li>
+                        <li><a href="">Technologies</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div className='flex items-center gap-3 mr-20'>
+                    <button className='bg-white text-black w-[92px] h-[39px] rounded-full'>Sign in</button>
+                    <button className='bg-[#d91b7e] text-white w-[92px] h-[39px] rounded-full'>Sign up</button>
+                </div>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
